21824644afb867b5eae2c2f58064d47d9b8f0bca
Disidd <dmitriy.sidorenko97@gmail.com>
2024-07-24 19:26:52 +0200

    numbers and strings

---
+++ HM2/index.js
@@ -0,0 +1,7 @@
+color = prompt ('What color do you like? ');
+country = prompt ('Where are you from?');
+profession = prompt ('What`s your profession?');
+alert (`My favorite color ${color}. I'm from ${country}.  My profession is a ${profession}`)
+
+FiveDigitNumber = prompt ('Enter a five-digit number');
+console.log(FiveDigitNumber.replaceAll('', ' '));
