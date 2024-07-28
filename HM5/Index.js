3a4e0a69ee2acbd8e35bbdaf12669268617cfd70
Disidd <dmitriy.sidorenko97@gmail.com>
2024-07-28 16:23:11 +0200

    HM5

---
+++ HM5/Index.js
@@ -0,0 +1,7 @@
+firstNumber = +prompt('Enter first number');
+secondNumber = +prompt('Enter second number');
+thirdNumber = +prompt('Enter third number');
+
+arithmeticAverage = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(arithmeticAverage)
